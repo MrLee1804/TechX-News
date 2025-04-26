3dbca397bd3312118a2681ba941b6794e087f52a
MrLee1804 <lingeshwaran3535@gmail.com>
2025-04-26 20:55:26 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -20,7 +20,7 @@ var newsData = [];
 
 
 // API key
-const apikey = "YOUR_NEWS_API_KEY" <!-- REPLACE THIS PART -->
+const apikey = "YOUR_NEWS_API_KEY" // REPLACE THIS PART
 const Headlines = "https://newsapi.org/v2/top-headlines?country=us&apiKey="
 const General = "https://newsapi.org/v2/top-headlines?country=us&category=general&apiKey="
 const Business = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey="
